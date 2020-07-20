070b57ccbf1a0ffe16d924484889a4b805445565
Paul Van Leuven <paulcharlesvl@gmail.com>
2020-07-20 14:21:39 -0500

    getting closer

--- techProject5/js/main.js
+++ techProject5/js/main.js
@@ -1,15 +1,15 @@
-let captions = [];
+// get input from user
 
-$(".image-grid > a").each(function(i) {
-  captions[i] = $(this).attr("data-title");
-});
+document.getElementById("search").addEventListener("keyup", getUserInput);
 
-document.getElementById("search").addEventListener('keyup', myFunction);
-
-function myFunction() {
-    var x = document.getElementById("search");
-    x.value = x.value.toLowerCase();
+function getUserInput() {
+    var text = document.getElementById("search");
+    text.value = text.value.toLowerCase();
 }
 
-var caption = document.getElementsByTagName("a")[0].getAttribute("data-title").toLowerCase();
 
+// get captions
+
+var caption = document.getElementsByTagName("a")[0].getAttribute("data-title");
+
+caption = caption.toLowerCase();
