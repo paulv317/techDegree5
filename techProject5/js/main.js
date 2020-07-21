d0ff1c7dec7c377657a40bfc821631342b4c25f2
Paul Van Leuven <paulcharlesvl@gmail.com>
2020-07-20 20:50:44 -0500

    js files updated and fixed

--- techProject5/js/main.js
+++ techProject5/js/main.js
@@ -1,15 +1,23 @@
-// get input from user
-
-document.getElementById("search").addEventListener("keyup", getUserInput);
-
-function getUserInput() {
-    var text = document.getElementById("search");
-    text.value = text.value.toLowerCase();
+//add an event listener to watch key ups on the search input 
+document.querySelector("#search").addEventListener("keyup", function () {
+    
+//store the search value in a variable and set it to lower case
+const input = document.querySelector("#search").value.toLowerCase();
+      
+//store photo elements in array 
+const photos = document.querySelectorAll(".list a");
+//loop over each photo 
+for (let i = 0; i < photos.length; i++){
+//store the caption of a the photo at an array index and make it lowercase 
+const caption = photos[i].getAttribute("data-title").toLowerCase();
+//if the search term is in the caption
+if (caption.includes(input)) {
+//show the image
+$(photos[i]).show();
+} 
+//otherwise hide them 
+else {
+  $(photos[i]).hide();
 }
-
-
-// get captions
-
-var caption = document.getElementsByTagName("a")[0].getAttribute("data-title");
-
-caption = caption.toLowerCase();
+}
+});
